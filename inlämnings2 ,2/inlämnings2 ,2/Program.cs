6ca8732aa8728dd234d40ac131644f6460286c73
Admin <Admin@TEBELEVDISK>
2019-10-21 12:27:51 +0200

    skrivit metod för att ska skriva medelvärdet för tre tal

---
+++ inlämnings2 ,2/inlämnings2 ,2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inlämnings2__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tal1 = 2.4;
+            double tal2 = 4.2;
+            double tal3 = 3.2;
+            double MedelVärdet = BeräknaMedel(tal1, tal2, tal3);
+            Console.WriteLine("Medelvärdet: " + MedelVärdet);
+        }
+
+        static double BeräknaMedel(double tal1, double tal2, double tal3)
+        {
+            return (tal1 + tal2 + tal3) / 3;
+        }
+    }
+}
+
